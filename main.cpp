37b247ee2972ae726c4ae0181f1153d409d64ff7
dray <drayparish@gmail.com>
2025-06-02 08:02:40 -0500

    working on hooking main.cpp up to API

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 // main.cpp : main functionality for trading bot
 
-#include "TwsApi/EClientL0.h"
-#include "TwsApi/EWrapperL0.h"
-#include "source/PosixClient/Shared/Contract.h"
+#include "Api/TwsApiL0.h"
 #include "source/PosixClient/Shared/Order.h"
 #include <iostream>
+
+// build custom wrapper to EWrapper:
+class MyEWrapper : public EWrapperL0 {
+    public:
+    void tickPrice(TickerId tickerId, TickType field, double price, int canAutoExecute) override {
+        // TODO: add implementation
+    }
+};
